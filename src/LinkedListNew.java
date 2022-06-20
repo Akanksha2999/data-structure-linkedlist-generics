65623fbe715de6bf4407b6bf097afbe78dbef2a8
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-20 14:43:42 +0530

    ADDED: Created linked list

---
+++ src/LinkedListNew.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class LinkedListNew {
+    public static void main(String[] args) {
+
+        //Linked list created
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        System.out.println("Elements in the list are:" + list);
+    }
+}
